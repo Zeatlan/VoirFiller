e07a27d3e772dd30ce59fa861a882f70f8b4e0af
Zeatlan <salimalexo@gmail.com>
2024-09-23 15:09:29 +0200

    feat: dynamic styling

--- src/content.ts
+++ src/content.ts
@@ -11,20 +11,29 @@ function injectFillerInfo(episodeType: EpisodeType) {
     const div = document.querySelector('.c-blog-post');
 
     const infoElement = document.createElement('div');
-    infoElement.style.cssText = `
-        background-color: #d85151;
-        color: white;
-        padding: 8px 12px;
-        border-radius: 4px;
-    `;
+    let bgColor = '';
 
     if(episodeType === 'Filler') {
         infoElement.textContent = 'Vous êtes en train de regarder un filler.';
+        bgColor = '#d85151';
     }else if(episodeType === 'Manga Canon') {
         infoElement.textContent = 'Vous êtes en train de regarder un épisode canon.';
+        bgColor = '#51d88a';
     }else if(episodeType === 'Mixed Canon/Filler') {
         infoElement.textContent = 'Vous êtes en train de regarder un épisode canon/filler.';
+        bgColor = '#d8b751';
     }
+
+    infoElement.style.cssText = `
+        background-color: ${bgColor};
+        color: white;
+        padding: 8px 12px;
+        border-radius: 4px;
+        text-align: center;
+        font-size: 18px;
+        font-weight: 600;
+        font-family: 'Montserrat', sans-serif;
+    `;
 
     div?.appendChild(infoElement); 
 }
